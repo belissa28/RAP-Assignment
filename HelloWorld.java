ea911dbf05c9c7a305bec771fc42caf8c2ddd50e
belissabaez <baezb@union.edu>
2021-05-20 15:07:36 -0400

    adding HelloWorld.java program and HelloWorld.class

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+//package com.revature.mypackage;
+//Assignment 1-RAP
+//Belissa Baez
+//short program that prints out Hello World
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
